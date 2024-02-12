956785ea6003e8f421912a76d4427b67717a34fc
FihFafnir <fihfdragon@gmail.com>
2024-02-12 12:19:40 -0300

    Updated

--- Matemáticos/swap.cpp
+++ Matemáticos/swap.cpp
@@ -4,7 +4,7 @@ using namespace std;
 void swap(int& first, int& second) {
     if (first == second)
         return;
-        
+
     first ^= second;
     second ^= first;
     first ^= second;
@@ -13,6 +13,6 @@ void swap(int& first, int& second) {
 int main(int argc, char** argv) {
     int x, y;
     scanf("%d %d", &x, &y);
-    swap(&x, &y);
+    swap(x, y);
     printf("A: %d\nB: %d\n", x, y);
 }
