2415d597f3ae82bbbf945765cc864ffebaf8079c
FihFafnir <fihfdragon@gmail.com>
2024-03-02 20:08:08 -0300

    Updated

--- Ordenação/quick.cpp
+++ Ordenação/quick.cpp
@@ -2,45 +2,52 @@
 
 using namespace std;
 
-void swap(int& first, int& second) {
+void swap(int &first, int &second) {
     int tmp = first;
     first = second;
     second = tmp;
 };
 
 template <size_t S>
-int getPivot(int (&arr)[S], size_t start, size_t end) {
+size_t getPivotIndex(int (&arr)[S], size_t start, size_t end) {
     size_t middle = (end + start) >> 1;
-    int maxValue = max(max(arr[end - 1], arr[middle]), arr[start]);
-    int minValue = min(min(arr[end - 1], arr[middle]), arr[start]);
 
-    return arr[start] + arr[middle] + arr[end - 1] - maxValue - minValue;
+    if (arr[start] < arr[middle]) {
+        if (arr[middle] < arr[end])
+            return middle;
+        return arr[start] < arr[end] ? end : start;
+    }
+
+    if (arr[start] < arr[end])
+        return start;
+    return arr[middle] < arr[end] ? end : middle;
 }
 
 template <size_t S>
-void quickSort(int (&arr)[S], size_t start = 0, size_t end = S) {
-    int pivot = getPivot(arr, start, end);
-    size_t left = start, right = end - 1;
+void quickSort(int (&arr)[S], size_t start = 0, size_t end = S - 1) {
+    size_t pivot = getPivotIndex(arr, start, end);
+    size_t left = start, right = end;
 
-    while (left < right) {
-        while (pivot < arr[right])
+    while (left <= right) {
+        while (arr[pivot] < arr[right])
             right--;
-        while (pivot > arr[left])
+        while (arr[pivot] > arr[left])
             left++;
 
-        if (left < right)
-            swap(arr[left], arr[right]);
+        if (left <= right)
+            swap(arr[left++], arr[right--]);
     }
 
-    if (start < left)
-        quickSort(arr, start, left);
-    if (left + 1 < end)
-        quickSort(arr, left + 1, end);
+    if (start < right)
+        quickSort(arr, start, right);
+    if (left < end)
+        quickSort(arr, left, end);
 }
 
-
-int main(int argc, char** argv) {
+int main(int argc, char **argv) {
     int arr[] = {4, 7, 2, 6, 9, 1, 8, 3};
     quickSort(arr);
+    for (int &i : arr)
+        cout << i << endl;
     return 0;
 }
